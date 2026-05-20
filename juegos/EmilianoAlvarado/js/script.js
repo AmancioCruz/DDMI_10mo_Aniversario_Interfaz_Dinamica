50996adc6c0acc005477a9d799eace934c701e7c
Emiliano <al248146@alumnos.uacj.mx>
2026-05-20 12:40:58 -0600

    progreso

--- juegos/EmilianoAlvarado/js/script.js
+++ juegos/EmilianoAlvarado/js/script.js
@@ -29,19 +29,39 @@ class Plataforma {
         this.alto = alto
         this.velocidad = velocidad
     }
+    draw(context){
+        context.fillStyle = "orange";
+        context.fillRect(this.x, this.y, this.ancho, this.alto);
+    }
+    mover(direcccion){
+        this.x += this.velocidad * direcccion;
+    }
 }
 
 const canvas = document.getElementById("canvas");
 const context = canvas.getContext("2d");
 
 const bola = new Bola(200, 200, 10, 2, 2);
-bola.draw(context);
+const plataforma = new Plataforma(175, canvas.clientHeight-20,70,10,20);
+
+
+document.addEventListener("keydown",(event)=>{
+    if(event.key === "ArrowLeft"){
+        plataforma.mover(-1);
+    } else if(event.key ==="ArrowRight"){
+        plataforma.mover(1);
+    }
+})
 
 function gameLoop(){
     context.clearRect(0,0,canvas.clientWidth, canvas.clientHeight)
 
     bola.update()
     bola.draw(context);
+
+    if(bola.x -)
+
+    plataforma.draw(context);
 
     requestAnimationFrame(gameLoop);
 }
